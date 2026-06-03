7399c4d77f4de9fdda445bee766f4fc7516edd06
Luís Guilherme <luis.bl.guilherme@gmail.com>
2026-06-03 18:49:28 -0300

    test_lista-ox.c

---
+++ test_lista-ox.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include "function_validar.c"
+
+int main()
+{
+    printf("Teste X: %d\n", validar('X'));
+    printf("Teste O: %d\n", validar('O'));
+    printf("Teste A: %d\n", validar('A'));
+
+    printf("Criado por Luís Guilherme\n");
+    return 0;
+}
